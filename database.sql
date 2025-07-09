feed6b11068502d91d2cae96999c2b7c5d196b5a
AlexBD27 <109763600+AlexBD27@users.noreply.github.com>
2025-07-09 08:50:14 -0500

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,34 @@
+
+CREATE TABLE procedencia(
+  id INT PRIMARY KEY,
+  distrito varchar(255) NOT NULL,
+  provincia varchar(255) NOT NULL,
+  departamento varchar(255) NOT NULL,
+);
+
+CREATE TABLE pacientes(
+  id INT PRIMARY KEY,
+  nombre varchar(255) not null,
+  ap_paterno varchar(255) not null,
+  ap_materno varchar(255) not null,
+  fec_nacimiento DATE,
+  edad INT GENERATED ALWAYS AS (TIMESTAMPDIFF(YEAR, fec_nacimiento, CURDATE())) STORED,
+  telefono varchar(255),
+  id_procedencia INT,
+
+  FOREIGN KEY (id_procedencia) REFERENCES procedencia(id)
+);
+
+INSERT INTO procedencia(id, distrito, provincia, departamento)
+VALUES (1, 'Nuevo Chimbote', 'Santa', 'Ancash'),
+(2, 'Cajamarca', 'Cajamarca', 'Cajamarca'),
+(3, 'Casma', 'Santa', 'Ancash'),
+(4, 'Huaraz', 'Huaraz', 'Ancash'),
+(5, 'La Esperanza', 'Trujillo', 'La Libertad');
+
+INSERT INTO pacientes(id, nombre, ap_paterno, ap_materno, fec_nacimiento, telefono, id_procedencia)
+VALUES (1, 'Pedro'. 'Martinez', 'Lopez', '1990-05-16', '958459652', 1),
+(2, 'Juan'. 'Fernandez', 'Aguirre', '1995-05-16', '954856321', 2),
+(3, 'Luz'. 'Morales', 'Andrada', '1980-05-12', '954585632', 3),
+(4, 'Fernando'. 'Palomo', 'Lopez', '1970-05-16', '958459652', 4),
+(5, 'Lucero'. 'Palomino', 'Lopez', '2000-05-16', '968563254', 5);
