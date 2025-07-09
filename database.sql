d5b068bfbb730c471665cc40aa37610dd93132d5
AlexBD27 <109763600+AlexBD27@users.noreply.github.com>
2025-07-09 09:52:21 -0500

    Update database.sql

--- database.sql
+++ database.sql
@@ -14,7 +14,7 @@ CREATE TABLE pacientes(
   ap_paterno varchar(255) not null,
   ap_materno varchar(255) not null,
   fec_nacimiento DATE,
-  edad INT,
+  edad INT GENERATED ALWAYS AS (TIMESTAMPDIFF(YEAR, fec_nacimiento, CURDATE())) STORED,
   telefono1 varchar(255) NOT NULL,
   telefono2 varchar(255) NULL,
   id_procedencia INT,
@@ -29,11 +29,11 @@ VALUES (1, 'Nuevo Chimbote', 'Santa', 'Ancash'),
 (4, 'Huaraz', 'Huaraz', 'Ancash'),
 (5, 'La Esperanza', 'Trujillo', 'La Libertad');
 
-INSERT INTO pacientes(id, nombre, ap_paterno, ap_materno, fec_nacimiento, edad, telefono1, telefono2, id_procedencia)
-VALUES (1, 'Pedro', 'Martinez', 'Lopez', '1990-05-16', 35, '958459652', null, 1),
-(2, 'Juan', 'Fernandez', 'Aguirre', '1995-05-16', 30, '954856321', '957854625', 2),
-(3, 'Luz', 'Morales', 'Andrada', '1980-05-12', 45, '954585632', '968745321', 3),
-(4, 'Fernando', 'Palomo', 'Lopez', '1970-05-16', 55, '958459652', null, 4),
-(5, 'Alex', 'Tirado', 'Mendoza', '2003-07-27', 21, '958459652', null, 2),
-(6, 'Almendra', 'Perez', 'Lopez', '1970-05-16', 55, '958459652', null, 4),
-(7, 'Lucero', 'Palomino', 'Lopez', '2000-05-16', 25, '968563254', null, 5);
+INSERT INTO pacientes(id, nombre, ap_paterno, ap_materno, fec_nacimiento, telefono1, telefono2, id_procedencia)
+VALUES (1, 'Pedro', 'Martinez', 'Lopez', '1990-05-16', '958459652', null, 1),
+(2, 'Juan', 'Fernandez', 'Aguirre', '1995-05-16', '954856321', '957854625', 2),
+(3, 'Luz', 'Morales', 'Andrada', '1980-05-12', '954585632', '968745321', 3),
+(4, 'Fernando', 'Palomo', 'Lopez', '1970-05-16', '958459652', null, 4),
+(5, 'Alex', 'Tirado', 'Mendoza', '2003-07-27', '958459652', null, 2),
+(6, 'Almendra', 'Perez', 'Lopez', '1970-05-16', '958459652', null, 4),
+(7, 'Lucero', 'Palomino', 'Lopez', '2000-05-16', '968563254', null, 5);
